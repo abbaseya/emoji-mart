82af2e2950c1073b4ec47cbad6a5ef872fa61720
Ahmed Abbas <a.abbas@ixdc.net>
2022-08-24 17:56:00 +0200

    relative packages

--- packages/emoji-mart-react/react.js
+++ packages/emoji-mart-react/react.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react'
-import { Picker } from 'emoji-mart'
+import { Picker } from '../emoji-mart'
 
 export default function EmojiPicker(props) {
   const ref = useRef(null)
